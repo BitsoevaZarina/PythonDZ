ca910afa1b0c74f8b8e12c4a2f7e7d5ee6b70c51
BitsoevaZarina <zarina06@yandex.ru>
2022-03-17 22:20:32 +0300

    Lesson1dz

---
+++ задание1.py
@@ -0,0 +1,46 @@
+# задание 1
+
+# переменные с типом данных строка
+
+name = 'Adelaida'
+name_1 = 'Aglina'
+data = '14.02.22'
+data_1 = '13.02.22'
+print(type(name))
+print(type(name_1))
+print(type(data))
+print(type(data_1))
+print(name, data)
+print(name_1, data_1)
+print(name + data)
+print(name_1 * 7)
+name = input('name: ')
+data = input('data: ')
+
+# переменные с типом данных числа целые и десятичные
+
+a = 20
+b = 4
+print(a + b)
+print(a * b)
+print(a / b)
+a = input('a: ')
+b = input('b: ')
+c = a + b
+print(c)
+a = int(a)
+b = int(b)
+c = a + b
+print(c)
+d = a ** b
+print(d)
+n = input('n: ')
+m = input('m: ')
+n = float(n)
+m = float(m)
+k = n * m
+l = n / m
+p = n - m
+print(k)
+print(l)
+print(p)
